c71cf9abc96d2ec12b986ad75501f8ed72e67287
Ari <abenderlong@gmail.com>
2016-03-12 15:24:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 const moment = require('moment');
-console.log(chalk.cyan(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] Booting...`));
+console.log(chalk.cyan('[${moment().format('YYYY-MM-DD HH:mm:ss')}] Booting...'));
 
 if (process.env.DEV) {
   require('babel-register');
